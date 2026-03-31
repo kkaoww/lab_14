3dbe969f724ebfdbe77d484a7dd4ce6a16b48d26
kkaow <120167981+kkaoww@users.noreply.github.com>
2026-03-31 14:35:51 +0700

    finish

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i=1; i<10; i++)   
+    {  
+        T keep = d[i];  
+        int j= i-1;  
+        while(j>=0 && keep >= d[j])  
+        {  
+            d[j+1] = d[j];   
+            j = j-1;  
+        }  
+        d[j+1] = keep;
+        cout<<"Pass "<<i<<":";
+		for(int z=0;z<10;z++){
+			cout<<d[z]<<" ";
+		}
+        cout<<"\n";
+    }  
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
